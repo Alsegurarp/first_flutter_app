43ead637743d471eed809354faa5dc227491ee0c
Alsegurarp <alsegurarp@gmail.com>
2025-11-07 17:52:09 -0600

    avance y mas estilo

--- lib/models/questions_summary.dart
+++ lib/models/questions_summary.dart
@@ -10,43 +10,134 @@ class QuestionsSummary extends StatelessWidget {
     return SizedBox(
       height: 300,
       child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
           children: summaryData.map((data) {
-            return Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.green,
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    ((data['question_index'] as int? ?? 0) + 1).toString(),
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data['question'] as String? ?? 'No question provided',
+            return Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(15.0),
+              ),
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              color: const Color.fromARGB(255, 221, 217, 185),
+              child: Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green,
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 52, 134, 55),
+                        width: 2,
                       ),
-                      const SizedBox(height: 5),
-                      Text(
-                        data['user_answer'] as String? ?? 'No answer provided',
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        data['correct_answer'] as String? ??
-                            'No correct answer provided',
-                      ),
-                    ],
+                    ),
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(right: 10, left: 10),
+                    child: Text(
+                      ((data['question_index'] as int? ?? 0) + 1).toString(),
+                      style: const TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(width: 5),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 2,
+                        children: [
+                          SizedBox(height: 5),
+                          Text(
+                            data['question'] as String? ??
+                                'No question provided',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text('Tu respuesta:'),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                          8.0,
+                                        ),
+                                        color: const Color.fromARGB(
+                                          185,
+                                          35,
+                                          57,
+                                          182,
+                                        ),
+                                      ),
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        data['user_answer'] as String? ??
+                                            'No answer provided',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text('Respuesta correcta:'),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                          8.0,
+                                        ),
+                                        color: const Color.fromARGB(
+                                          101,
+                                          105,
+                                          240,
+                                          175,
+                                        ),
+                                      ),
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        data['correct_answer'] as String? ??
+                                            'No correct answer provided',
+                                        style: const TextStyle(
+                                          color: Color.fromARGB(
+                                            255,
+                                            255,
+                                            255,
+                                            255,
+                                          ),
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           }).toList(),
         ),
